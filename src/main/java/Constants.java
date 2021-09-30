6122a1c691b0916a88d98b07a549eeddc949000e
mao0420 <iwanaga0420@gmail.com>
2021-09-30 18:42:11 +0900

    定数の内容を一部修正

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -29,18 +29,18 @@ public class Constants {
             """;
     public static final String DISPLAY_BUTTON_BACK_TO_TITLE = "タイトルに戻る";
     public static final String DISPLAY_TEXT_GAME_CLEAR_RESULT = """
-            <html><center>ゲームクリア！<br/>
+            <html><body align=center>ゲームクリア！<br/>
             <br/>
             正解の数字は%sでした！<br/>
             <br/>
-            入力回数:%d回<br/></center></html>
+            入力回数:%d回<br/></body align=center></html>
             """;
     public static final String DISPLAY_TEXT_GAME_OVER_RESULT = """
-            <html><center>ゲームクリア！<br/>
+            <html><body align=center>ゲームクリア！<br/>
             <br/>
             正解の数字は%sでした！<br/>
             <br/>
-            入力回数:%d回<br/><center></html>
+            入力回数:%d回<br/></body align=center></html>
             """;
     public static final String FONT_MS_GOTHIC = "ＭＳ ゴシック";
     public static final String CARD_TITLE_SCREEN = "CardTitleScreen";
