2c4fb7ee7f70af3da5b3e70a817728bdf00f6b38
mao0420 <iwanaga0420@gmail.com>
2021-09-30 18:26:31 +0900

    ゲームクリア画面を追加、確認用の一時的にタイトル画面にボタンを追加

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static final String DISPLAY_BUTTON_GAME_START = "ゲームスタート";
     public static final String DISPLAY_BUTTON_RULE_DESCRIPTION = "ルール説明";
     public static final String DISPLAY_BUTTON_GAME_END = "ゲーム終了";
+    public static final String DISPLAY_BUTTON_GAME_CLEAR = "ゲームクリア画面";
     public static final String DISPLAY_TEXT_RULE_DESCRIPTION = """
             <html><body>ヒット&ブローはプログラム側がランダムで設定した数字を当てるゲームです。<br/>
             <br/>
@@ -15,7 +16,7 @@ public class Constants {
             ブロー:数字は合っているが、桁の位置が違う数字の数です。<br/>
             <br/>
             例:正解が[083]入力が[385]の時、<br/>
-            　 8は桁の位置も数字も合っている為ヒット、<br/>
+            　 8は桁も数字も合っている為ヒット、<br/>
             　 3は桁の位置が違うが数字は合っている為ブローとなり、<br/>
             　 ヒットとブローの数がそれぞれ1つずつの為、ヒット:1 ブロー:1と表示されます。<br/>
             <br/>
@@ -26,8 +27,23 @@ public class Constants {
             また、数値入力時にGを入力するとギブアップとしてゲームを終了する事が出来ます。<br/>
             <br/></body></html>
             """;
-    public static final String DISPLAY_BUTTON_BACK = "戻る";
+    public static final String DISPLAY_BUTTON_BACK_TO_TITLE = "タイトルに戻る";
+    public static final String DISPLAY_TEXT_GAME_CLEAR_RESULT = """
+            <html><body><center>ゲームクリア！<br/>
+            <br/>
+            正解の数字は%sでした！<br/>
+            <br/>
+            入力回数:%d回<br/></center></body></html>
+            """;
+    public static final String DISPLAY_TEXT_GAME_OVER_RESULT = """
+            <html><body>ゲームクリア！<br/>
+            <br/>
+            正解の数字は%sでした！<br/>
+            <br/>
+            入力回数:%d回<br/></body></html>
+            """;
     public static final String FONT_MS_GOTHIC = "ＭＳ ゴシック";
     public static final String CARD_TITLE_SCREEN = "CardTitleScreen";
     public static final String CARD_RULE_DESCRIPTION = "CardRuleDescription";
+    public static final String CARD_GAME_CLEAR = "CardGameClear";
 }
--- src/main/java/Contents.java
+++ src/main/java/Contents.java
@@ -16,7 +16,7 @@ public class Contents extends JFrame implements ActionListener {
     }
 
     public Contents() {
-        //CardTitleScreen
+        //タイトル画面カード
         JPanel CardTitleScreen = new JPanel();
         CardTitleScreen.setLayout(new BoxLayout(CardTitleScreen, BoxLayout.Y_AXIS));
 
@@ -40,7 +40,15 @@ public class Contents extends JFrame implements ActionListener {
         ButtonTitleScreenGameEnd.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         ButtonTitleScreenRuleDescription.addActionListener(this);
-        ButtonTitleScreenRuleDescription.setActionCommand("CardRuleDescription");
+        ButtonTitleScreenRuleDescription.setActionCommand(Constants.CARD_RULE_DESCRIPTION);
+
+        //ゲームクリア画面デバッグ用
+        JButton ButtonTitleScreenGameClear = new JButton(Constants.DISPLAY_BUTTON_GAME_CLEAR);
+        ButtonTitleScreenGameClear.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 20));
+        ButtonTitleScreenGameClear.setPreferredSize(new Dimension(300, 50));
+        ButtonTitleScreenGameClear.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ButtonTitleScreenGameClear.addActionListener(this);
+        ButtonTitleScreenGameClear.setActionCommand(Constants.CARD_GAME_CLEAR);
 
         CardTitleScreen.add(Box.createGlue());
         CardTitleScreen.add(LabelTitleScreenTitle);
@@ -52,16 +60,18 @@ public class Contents extends JFrame implements ActionListener {
         CardTitleScreen.add(Box.createGlue());
         CardTitleScreen.add(ButtonTitleScreenGameEnd);
         CardTitleScreen.add(Box.createGlue());
+        //ゲームクリア画面デバッグ用
+        CardTitleScreen.add(ButtonTitleScreenGameClear);
 
         Container contentPane01 = getContentPane();
         contentPane01.add(CardTitleScreen, BorderLayout.CENTER);
 
-        //CardRuleDescription
+        //ルール説明画面カード
         JPanel CardRuleDescription = new JPanel();
         CardRuleDescription.setLayout(new BoxLayout(CardRuleDescription, BoxLayout.Y_AXIS));
 
         JLabel LabelRuleDescriptionRule = new JLabel(Constants.DISPLAY_TEXT_RULE_DESCRIPTION);
-        JButton ButtonRuleDescriptionBack = new JButton(Constants.DISPLAY_BUTTON_BACK);
+        JButton ButtonRuleDescriptionBack = new JButton(Constants.DISPLAY_BUTTON_BACK_TO_TITLE);
 
         LabelRuleDescriptionRule.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 18));
         ButtonRuleDescriptionBack.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 30));
@@ -81,20 +91,46 @@ public class Contents extends JFrame implements ActionListener {
         Container contentPane02 = getContentPane();
         contentPane02.add(CardRuleDescription, BorderLayout.CENTER);
 
+        //ゲームクリア画面カード
+        JPanel CardGameClear = new JPanel();
+        CardGameClear.setLayout(new BoxLayout(CardGameClear, BoxLayout.Y_AXIS));
 
+        JLabel LabelGameClearResult = new JLabel(Constants.DISPLAY_TEXT_GAME_CLEAR_RESULT);
+        JButton ButtonGameClearBackToTitle = new JButton(Constants.DISPLAY_BUTTON_BACK_TO_TITLE);
+
+        LabelGameClearResult.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 30));
+        ButtonGameClearBackToTitle.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 30));
+
+        LabelGameClearResult.setAlignmentX(CENTER_ALIGNMENT);
+        LabelGameClearResult.setHorizontalAlignment(JLabel.CENTER);
+        ButtonGameClearBackToTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        ButtonGameClearBackToTitle.addActionListener(this);
+        ButtonGameClearBackToTitle.setActionCommand(Constants.CARD_TITLE_SCREEN);
+
+        CardGameClear.add(Box.createGlue());
+        CardGameClear.add(LabelGameClearResult);
+        CardGameClear.add(Box.createGlue());
+        CardGameClear.add(ButtonGameClearBackToTitle);
+        CardGameClear.add(Box.createGlue());
+
+        Container contentPane04 = getContentPane();
+        contentPane04.add(CardGameClear, BorderLayout.CENTER);
+
+        //カード設定
         cardPanel = new JPanel();
         layout = new CardLayout();
         cardPanel.setLayout(layout);
 
         cardPanel.add(CardTitleScreen, Constants.CARD_TITLE_SCREEN);
         cardPanel.add(CardRuleDescription, Constants.CARD_RULE_DESCRIPTION);
+        cardPanel.add(CardGameClear, Constants.CARD_GAME_CLEAR);
 
         getContentPane().add(cardPanel, BorderLayout.CENTER);
     }
 
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
-
         layout.show(cardPanel, cmd);
     }
 }
