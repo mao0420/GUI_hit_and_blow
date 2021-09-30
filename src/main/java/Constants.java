ef84196f058e696bd303064e6a31d678151c8c76
mao0420 <iwanaga0420@gmail.com>
2021-09-30 16:10:21 +0900

    変数名の変更と文字列の定数化を実施

---
+++ src/main/java/Constants.java
@@ -0,0 +1,33 @@
+public class Constants {
+    public static final String DISPLAY_NAME_FRAME_TITLE = "Hit & Blow";
+    public static final String DISPLAY_TEXT_TITLE = "Hit & Blow";
+    public static final String DISPLAY_BUTTON_GAME_START = "ゲームスタート";
+    public static final String DISPLAY_BUTTON_RULE_DESCRIPTION = "ルール説明";
+    public static final String DISPLAY_BUTTON_GAME_END = "ゲーム終了";
+    public static final String DISPLAY_TEXT_RULE_DESCRIPTION = """
+            <html><body>ヒット&ブローはプログラム側がランダムで設定した数字を当てるゲームです。<br/>
+            <br/>
+            このプログラムでは被り無しの0～9の数字が3桁設定されます。<br/>
+            ユーザー側が3桁の数字を入力し、<br/>
+            その数字と正解の数字を比較して次のヒントが表示されます。<br/>
+            <br/>
+            ヒット:桁の位置も数字も合っている数字の数です。<br/>
+            ブロー:数字は合っているが、桁の位置が違う数字の数です。<br/>
+            <br/>
+            例:正解が[083]入力が[385]の時、<br/>
+            　 8は桁の位置も数字も合っている為ヒット、<br/>
+            　 3は桁の位置が違うが数字は合っている為ブローとなり、<br/>
+            　 ヒットとブローの数がそれぞれ1つずつの為、ヒット:1 ブロー:1と表示されます。<br/>
+            <br/>
+            その後、再度数字の入力から繰り返し、3桁全部の数字を当てた場合はゲームクリアです。<br/>
+            少ない回数でのクリアを目指してください。<br/>
+            <br/>
+            10回目の入力までに正解の数字を見つけられないとゲームオーバーになります。<br/>
+            また、数値入力時にGを入力するとギブアップとしてゲームを終了する事が出来ます。<br/>
+            <br/></body></html>
+            """;
+    public static final String DISPLAY_BUTTON_BACK = "戻る";
+    public static final String FONT_MS_GOTHIC = "ＭＳ ゴシック";
+    public static final String CARD_TITLE_SCREEN = "CardTitleScreen";
+    public static final String CARD_RULE_DESCRIPTION = "CardRuleDescription";
+}
--- src/main/java/Contents.java
+++ src/main/java/Contents.java
@@ -8,7 +8,7 @@ public class Contents extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         Contents frame = new Contents();
-        frame.setTitle("ヒット&ブロー");
+        frame.setTitle(Constants.DISPLAY_NAME_FRAME_TITLE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,104 +16,83 @@ public class Contents extends JFrame implements ActionListener {
     }
 
     public Contents() {
-        //panel_a
-        JPanel panel_a = new JPanel();
-        panel_a.setLayout(new BoxLayout(panel_a, BoxLayout.Y_AXIS));
+        //CardTitleScreen
+        JPanel CardTitleScreen = new JPanel();
+        CardTitleScreen.setLayout(new BoxLayout(CardTitleScreen, BoxLayout.Y_AXIS));
 
-        JLabel label_a1 = new JLabel("Hit & Blow");
-        JButton button_a1 = new JButton("ゲームスタート");
-        JButton button_a2 = new JButton("ルール説明");
-        JButton button_a3 = new JButton("ゲーム終了");
+        JLabel LabelTitleScreenTitle = new JLabel(Constants.DISPLAY_TEXT_TITLE);
+        JButton ButtonTitleScreenGameStart = new JButton(Constants.DISPLAY_BUTTON_GAME_START);
+        JButton ButtonTitleScreenRuleDescription = new JButton(Constants.DISPLAY_BUTTON_RULE_DESCRIPTION);
+        JButton ButtonTitleScreenGameEnd = new JButton(Constants.DISPLAY_BUTTON_GAME_END);
 
-        label_a1.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 100));
-        button_a1.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 20));
-        button_a2.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 20));
-        button_a3.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 20));
+        LabelTitleScreenTitle.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 100));
+        ButtonTitleScreenGameStart.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 20));
+        ButtonTitleScreenRuleDescription.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 20));
+        ButtonTitleScreenGameEnd.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 20));
 
-        button_a1.setPreferredSize(new Dimension(300, 50));
-        button_a2.setPreferredSize(new Dimension(400, 50));
-        button_a3.setPreferredSize(new Dimension(400, 50));
+        ButtonTitleScreenGameStart.setPreferredSize(new Dimension(300, 50));
+        ButtonTitleScreenRuleDescription.setPreferredSize(new Dimension(400, 50));
+        ButtonTitleScreenGameEnd.setPreferredSize(new Dimension(400, 50));
 
-        label_a1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button_a1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button_a2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button_a3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        LabelTitleScreenTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ButtonTitleScreenGameStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ButtonTitleScreenRuleDescription.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ButtonTitleScreenGameEnd.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        button_a2.addActionListener(this);
-        button_a2.setActionCommand("panel_b");
+        ButtonTitleScreenRuleDescription.addActionListener(this);
+        ButtonTitleScreenRuleDescription.setActionCommand("CardRuleDescription");
 
-        panel_a.add(Box.createGlue());
-        panel_a.add(label_a1);
-        panel_a.add(Box.createGlue());
-        panel_a.add(Box.createGlue());
-        panel_a.add(button_a1);
-        panel_a.add(Box.createGlue());
-        panel_a.add(button_a2);
-        panel_a.add(Box.createGlue());
-        panel_a.add(button_a3);
-        panel_a.add(Box.createGlue());
+        CardTitleScreen.add(Box.createGlue());
+        CardTitleScreen.add(LabelTitleScreenTitle);
+        CardTitleScreen.add(Box.createGlue());
+        CardTitleScreen.add(Box.createGlue());
+        CardTitleScreen.add(ButtonTitleScreenGameStart);
+        CardTitleScreen.add(Box.createGlue());
+        CardTitleScreen.add(ButtonTitleScreenRuleDescription);
+        CardTitleScreen.add(Box.createGlue());
+        CardTitleScreen.add(ButtonTitleScreenGameEnd);
+        CardTitleScreen.add(Box.createGlue());
 
         Container contentPane01 = getContentPane();
-        contentPane01.add(panel_a, BorderLayout.CENTER);
+        contentPane01.add(CardTitleScreen, BorderLayout.CENTER);
 
-        //panel_b
-        JPanel panel_b = new JPanel();
-        panel_b.setLayout(new BoxLayout(panel_b, BoxLayout.Y_AXIS));
+        //CardRuleDescription
+        JPanel CardRuleDescription = new JPanel();
+        CardRuleDescription.setLayout(new BoxLayout(CardRuleDescription, BoxLayout.Y_AXIS));
 
-        JLabel label_b1 = new JLabel("""
-                <html><body>ヒット&ブローはプログラム側がランダムで設定した数字を当てるゲームです。<br/>
-                <br/>
-                このプログラムでは被り無しの0～9の数字が3桁設定されます。<br/>
-                ユーザー側が3桁の数字を入力し、<br/>
-                その数字と正解の数字を比較して次のヒントが表示されます。<br/>
-                <br/>
-                ヒット:桁の位置も数字も合っている数字の数です。<br/>
-                ブロー:数字は合っているが、桁の位置が違う数字の数です。<br/>
-                <br/>
-                例:正解が[083]入力が[385]の時、<br/>
-                　 8は桁の位置も数字も合っている為ヒット、<br/>
-                　 3は桁の位置が違うが数字は合っている為ブローとなり、<br/>
-                　 ヒットとブローの数がそれぞれ1つずつの為、ヒット:1 ブロー:1と表示されます。<br/>
-                <br/>
-                その後、再度数字の入力から繰り返し、3桁全部の数字を当てた場合はゲームクリアです。<br/>
-                少ない回数でのクリアを目指してください。<br/>
-                <br/>
-                10回目の入力までに正解の数字を見つけられないとゲームオーバーになります。<br/>
-                また、数値入力時にGを入力するとギブアップとしてゲームを終了する事が出来ます。<br/>
-                <br/></body></html>
-                """);
-        JButton button_b1 = new JButton("戻る");
+        JLabel LabelRuleDescriptionRule = new JLabel(Constants.DISPLAY_TEXT_RULE_DESCRIPTION);
+        JButton ButtonRuleDescriptionBack = new JButton(Constants.DISPLAY_BUTTON_BACK);
 
-        label_b1.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 18));
-        button_b1.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 30));
+        LabelRuleDescriptionRule.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 18));
+        ButtonRuleDescriptionBack.setFont(new Font(Constants.FONT_MS_GOTHIC, Font.BOLD, 30));
 
-        label_b1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button_b1.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        LabelRuleDescriptionRule.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ButtonRuleDescriptionBack.setAlignmentX(Component.RIGHT_ALIGNMENT);
 
-        button_b1.addActionListener(this);
-        button_b1.setActionCommand("panel_a");
+        ButtonRuleDescriptionBack.addActionListener(this);
+        ButtonRuleDescriptionBack.setActionCommand(Constants.CARD_TITLE_SCREEN);
 
-        panel_b.add(Box.createGlue());
-        panel_b.add(label_b1);
-        panel_b.add(Box.createGlue());
-        panel_b.add(button_b1);
-        panel_b.add(Box.createGlue());
+        CardRuleDescription.add(Box.createGlue());
+        CardRuleDescription.add(LabelRuleDescriptionRule);
+        CardRuleDescription.add(Box.createGlue());
+        CardRuleDescription.add(ButtonRuleDescriptionBack);
+        CardRuleDescription.add(Box.createGlue());
 
         Container contentPane02 = getContentPane();
-        contentPane02.add(panel_b, BorderLayout.CENTER);
+        contentPane02.add(CardRuleDescription, BorderLayout.CENTER);
 
 
         cardPanel = new JPanel();
         layout = new CardLayout();
         cardPanel.setLayout(layout);
 
-        cardPanel.add(panel_a, "panel_a");
-        cardPanel.add(panel_b, "panel_b");
+        cardPanel.add(CardTitleScreen, Constants.CARD_TITLE_SCREEN);
+        cardPanel.add(CardRuleDescription, Constants.CARD_RULE_DESCRIPTION);
 
         getContentPane().add(cardPanel, BorderLayout.CENTER);
     }
 
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
 
         layout.show(cardPanel, cmd);
